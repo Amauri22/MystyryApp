71f67033f380c3a2ff6e1c6bc845fe1e768613ae
Marlon Hamaekers <mar.hamaekers@gmail.com>
2023-03-28 14:06:02 +0200

    completed dice game

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
-let fileNames = [];
-for (let i = 1; i <= 6; i++) {
-  fileNames.push(`images/dice${i}.png`);
+// let fileNames = [];
+// for (let i = 1; i <= 6; i++) {
+//   fileNames.push(`images/dice${i}.png`);
+// }
+
+// console.log(fileNames);
+let randomGenerator = Math.random() * 6 + 1;
+let randomNumber1 = Math.floor(randomGenerator);
+let randomNumber2 = Math.floor(Math.random() * 6 + 1);
+// console.log(randomNumber1);
+// let myWindow = document.querySelectorAll("img")[1].getAttribute("class");
+
+// Ik kan hier ook een variabele van maken
+document
+  .querySelectorAll("img")[1]
+  .setAttribute("src", `images/dice${randomNumber1}.png`);
+
+document
+  .querySelectorAll("img")[2]
+  .setAttribute("src", `images/dice${randomNumber2}.png`);
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerText = "Player 1 wins";
+} else if (randomNumber1 < randomNumber2) {
+  document.querySelector("h1").innerText = "Player 2 wins";
+} else {
+  document.querySelector("h1").innerText = "Draw! Try again!";
 }
 
-console.log(fileNames);
-let randomGenerator = Math.random() * 6;
-let randomNumber1 = Math.floor(randomGenerator);
-console.log(randomNumber1);
-let myWindow = document.querySelectorAll("img")[1].getAttribute("class");
-document.querySelectorAll("img")[1];
-document.querySelectorAll("img")[1].setAttribute("src", "images/[9]");
-
-console.log(myWindow);
+// console.log(myWindow);
